b1c14e33845ba1d9602c81d6dd188400b1d68cee
sasuke21167 <49262776+sasuke21167@users.noreply.github.com>
2022-02-24 14:40:47 +0700

    add axios, product api, use product api in project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import {
   BrowserRouter,
   Redirect,
@@ -9,10 +9,31 @@ import {
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
 import "./App.scss";
+import productApi from "api/productApi";
 
 const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const params = {
+          _page: 1,
+          _limit: 10,
+        };
+        const response = await productApi.getAll(params);
+        console.log(response);
+        setProductList(response.data);
+      } catch (error) {
+        console.log("Fauled to fetch product list", error);
+      }
+    };
+
+    fetchProductList();
+  }, []);
+
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loading ...</div>}>
---
+++ src/features/Auth/pages/SignIn/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+SingIn.propTypes = {};
+
+function SingIn(props) {
+  return (
+    <div>
+      <div className="text-center">
+        <h2>Login Form</h2>
+
+        <p>or login with social accounts</p>
+      </div>
+    </div>
+  );
+}
+
+export default SingIn;
